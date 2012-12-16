db723cda8a63502a87abd385f24588116311fcfd
jp <donjeezy@gmail.com>
2012-12-15 22:09:55 -0500

    didn't mean to commit debug printf's

--- goraytest/main.go
+++ goraytest/main.go
@@ -5,7 +5,6 @@ import "image"
 import "image/png"
 import "image/color"
 import "os"
-import "fmt"
 
 func vecToNRGBA(v goray.Vector) color.NRGBA {
 	return color.NRGBA {
@@ -30,38 +29,22 @@ func main() {
 
 	light := goray.Vector{-1.0, 0.4, 0}
 
-
 	// 640 * 480 image
 	view := goray.View {cam, 640, 480}
-
 	bounds := image.Rect(0, 0, 640, 480)
-
 	img := image.NewNRGBA64(bounds)
-
 	
 	// render
 	for x := bounds.Min.X; x < bounds.Max.X; x++ {
 		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-
-			
 			ray := view.Ray(int16(x - 320), int16(-y + 240))
-
 			result := sphere.Trace(ray)
 			if result.Hit {
 				surfaceToLight := goray.Subtract(light, result.Intersection).Normalized()
 				diffuse := goray.LambertDiffuse(surfaceToLight, result.Normal)
 				shaded := result.Color.Scale(diffuse)
 				img.Set(x, y, vecToNRGBA(shaded))
-
-				if (x % 10 == 0) && (y % 10 == 0) {
-					fmt.Printf("pixel (%d, %d) ", x, y)
-					fmt.Printf("mapped (%d, %d) ", x - 320, -y + 240)
-					fmt.Println("ray ", ray)
-					fmt.Println(result)
-				}
-
 			}
-
 		}
 	}
 
